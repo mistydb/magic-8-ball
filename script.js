00681029db2d2cccd3be6c29ed70e073fe806466
mistydb <mistydbaird@gmail.com>
2020-08-28 22:47:31 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	
+var magic8Ball = {};
+
+magic8Ball.listOfAnswers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
+$("#answer").hide(); 
+	
+magic8Ball.askQuestion = function(question) {
+    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");    
+	$("#answer").fadeIn(4000);
+		var randomNumber = Math.random();
+        var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+        var randomIndex = Math.floor(randomNumberForListOfAnswers);
+        var answer = this.listOfAnswers[randomIndex];
+	
+	$("#answer").text( answer );
+	/*console.log(question);
+	console.log(answer);*/
+};
+	
+var onClick = function() {
+ 	$("#answer").hide();
+	$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+    setTimeout(
+		function(){
+	var question = prompt("ASK A YES/NO QUESTION!");
+	$("#8ball").effect( "shake" );
+	magic8Ball.askQuestion(question);
+		}, 500);
+};
+
+$("#questionButton").click( onClick );
+});
